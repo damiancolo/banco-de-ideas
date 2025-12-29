2a202b7653a455a88847f27f87e4e71cbb1e37d1
Damian Lafferranderie <damianlafferranderie@gmail.com>
2025-12-29 15:23:47 +0100

    Switch to Turbopack and specify Node version in package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
         ignoreBuildErrors: false,
-    },
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback = {
-                ...config.resolve.fallback,
-                fs: false,
-                path: false,
-                os: false,
-            };
-        }
-        return config;
     },
 };
 
